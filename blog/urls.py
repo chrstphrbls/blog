af50423c71b5ceb31545c80bc4dcb2c06a7f1add
chris <christopher.belaos@asti.dost.gov.ph>
2022-07-20 17:33:18 +0800

    first commit

---
+++ blog/urls.py
@@ -0,0 +1,11 @@
+# blog/urls.py
+
+from unicodedata import name
+from django.urls import path
+
+from .views import BlogListView, BlogDetailView #new
+
+urlpatterns = [
+    path{'post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'}, #new
+    path('', BlogListView.as_view(), name='home'),
+]
---
+++ blog/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.views.generic import ListView, DetailView #new
+
+from .models import Post
+
+class BlogListView(ListView):
+    model = Post
+    template_name = 'home.html'
+
+class BlogDetailView(DetailView): #new
+    model = Post 
+    template_name = 'post_detail.html'
