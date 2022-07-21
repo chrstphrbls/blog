756e78e2e55a575fddbfb8717fd5a877fd6fb865
chrstphrbls <109645414+chrstphrbls@users.noreply.github.com>
2022-07-21 09:21:34 +0800

    views added with error

--- blog/urls.py
+++ blog/urls.py
@@ -6,7 +6,7 @@ from django.urls import path
 from .views import BlogListView, BlogDetailView, BlogCreatView #new
 
 urlpatterns = [
-    path('post/new/', BlogCreateView.as_view(), name='post_new') #new
+    path('post/new/',BlogCreateView.as_view(), name='post_new'), #new
     path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'), 
     path('', BlogListView.as_view(), name='home'),
 ]
