499aa312cb3afff80f45f5f57c2cf56e9340359a
chrstphrbls <109645414+chrstphrbls@users.noreply.github.com>
2022-07-21 08:27:01 +0800

    edited the files

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,7 @@
+# blog/admin.py
+# error message when pressing the save button_no table found is the error
+
 from django.contrib import admin
-
-# Register your models here.
-
 from .models import Post
 
 admin.site.register(Post)
