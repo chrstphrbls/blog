abc0c306388a0c5cd4a866eb649fb76666bb15be
chrstphrbls <109645414+chrstphrbls@users.noreply.github.com>
2022-07-21 15:43:16 +0800

    adding user accounts

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'blog.apps.BlogConfig', #new
+    'blog.apps.BlogConfig', 
+    'accounts.apps.AccountConfig', #new
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
